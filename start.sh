1db6b39db29cafedb0ed605109a9c925581d09a8
Adrian Hernandez <adrianhdez929@gmail.com>
2020-10-24 14:05:17 -0400

    Refactored conffiles and logs

--- start.sh
+++ start.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 # --- crown mainnet: b8ebb3df (db = 0) ---
-/usr/bin/nice -n 19 python2.7 -u crawl.py conf/crawl.f9beb4d9.conf master > log/crawl.f9beb4d9.master.out 2>&1 &
-/usr/bin/nice -n 19 python2.7 -u crawl.py conf/crawl.f9beb4d9.conf slave > log/crawl.f9beb4d9.slave.1.out 2>&1 &
-/usr/bin/nice -n 19 python2.7 -u crawl.py conf/crawl.f9beb4d9.conf slave > log/crawl.f9beb4d9.slave.2.out 2>&1 &
+/usr/bin/nice -n 19 python2.7 -u crawl.py conf/crawl.conf.default master > log/crawl.b8ebb3df.master.out 2>&1 &
+/usr/bin/nice -n 19 python2.7 -u crawl.py conf/crawl.conf.default slave > log/crawl.b8ebb3df.slave.1.out 2>&1 &
+/usr/bin/nice -n 19 python2.7 -u crawl.py conf/crawl.conf.default slave > log/crawl.b8ebb3df.slave.2.out 2>&1 &
 
-/usr/bin/nice -n 19 python2.7 -u ping.py conf/ping.f9beb4d9.conf master > log/ping.f9beb4d9.master.out 2>&1 &
-/usr/bin/nice -n 19 python2.7 -u ping.py conf/ping.f9beb4d9.conf slave > log/ping.f9beb4d9.slave.1.out 2>&1 &
-/usr/bin/nice -n 19 python2.7 -u ping.py conf/ping.f9beb4d9.conf slave > log/ping.f9beb4d9.slave.2.out 2>&1 &
-/usr/bin/nice -n 19 python2.7 -u ping.py conf/ping.f9beb4d9.conf slave > log/ping.f9beb4d9.slave.3.out 2>&1 &
-/usr/bin/nice -n 19 python2.7 -u ping.py conf/ping.f9beb4d9.conf slave > log/ping.f9beb4d9.slave.4.out 2>&1 &
-/usr/bin/nice -n 19 python2.7 -u ping.py conf/ping.f9beb4d9.conf slave > log/ping.f9beb4d9.slave.5.out 2>&1 &
+/usr/bin/nice -n 19 python2.7 -u ping.py conf/ping.conf.default master > log/ping.b8ebb3df.master.out 2>&1 &
+/usr/bin/nice -n 19 python2.7 -u ping.py conf/ping.conf.default slave > log/ping.b8ebb3df.slave.1.out 2>&1 &
+/usr/bin/nice -n 19 python2.7 -u ping.py conf/ping.conf.default slave > log/ping.b8ebb3df.slave.2.out 2>&1 &
+/usr/bin/nice -n 19 python2.7 -u ping.py conf/ping.conf.default slave > log/ping.b8ebb3df.slave.3.out 2>&1 &
+/usr/bin/nice -n 19 python2.7 -u ping.py conf/ping.conf.default slave > log/ping.b8ebb3df.slave.4.out 2>&1 &
+/usr/bin/nice -n 19 python2.7 -u ping.py conf/ping.conf.default slave > log/ping.b8ebb3df.slave.5.out 2>&1 &
 
-/usr/bin/nice -n 19 python2.7 -u resolve.py conf/resolve.f9beb4d9.conf > log/resolve.f9beb4d9.out 2>&1 &
+/usr/bin/nice -n 19 python2.7 -u resolve.py conf/resolve.conf.default > log/resolve.b8ebb3df.out 2>&1 &
 
-/usr/bin/nice -n 19 python2.7 -u export.py conf/export.f9beb4d9.conf > log/export.f9beb4d9.out 2>&1 &
+/usr/bin/nice -n 19 python2.7 -u export.py conf/export.conf.default > log/export.b8ebb3df.out 2>&1 &
 
-/usr/bin/nice -n 19 python2.7 -u seeder.py conf/seeder.f9beb4d9.conf > log/seeder.f9beb4d9.out 2>&1 &
+/usr/bin/nice -n 19 python2.7 -u seeder.py conf/seeder.conf.default > log/seeder.b8ebb3df.out 2>&1 &
 
-/usr/bin/nice -n 19 python2.7 -u pcap.py conf/pcap.f9beb4d9.conf > log/pcap.f9beb4d9.1.out 2>&1 &
-/usr/bin/nice -n 19 python2.7 -u pcap.py conf/pcap.f9beb4d9.conf > log/pcap.f9beb4d9.2.out 2>&1 &
-/usr/bin/nice -n 19 python2.7 -u pcap.py conf/pcap.f9beb4d9.conf > log/pcap.f9beb4d9.3.out 2>&1 &
+/usr/bin/nice -n 19 python2.7 -u pcap.py conf/pcap.conf.default > log/pcap.b8ebb3df.1.out 2>&1 &
+/usr/bin/nice -n 19 python2.7 -u pcap.py conf/pcap.conf.default > log/pcap.b8ebb3df.2.out 2>&1 &
+/usr/bin/nice -n 19 python2.7 -u pcap.py conf/pcap.conf.default > log/pcap.b8ebb3df.3.out 2>&1 &
